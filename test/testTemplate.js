a232e84243fe2b07302f6ad0b806ec0e6d2542a7
Julian Cervantes <jicervantes@berkeley.edu>
2018-03-08 14:49:30 -0800

    Wrote tests

--- test/testTemplate.js
+++ test/testTemplate.js
@@ -2,28 +2,48 @@
 
 /* Add the dependencies you're testing */
 const Crowdsale = artifacts.require("./Crowdsale.sol");
+const Queue = artifacts.require("./Queue.sol");
+const Token = artifacts.require("./Token.sol")
 // YOUR CODE HERE
 
 contract('testTemplate', function(accounts) {
 	/* Define your constant variables and instantiate constantly changing 
 	 * ones
 	 */
-	const args = {};
-	let x, y, z;
+	const args = {_totalSupply = 10, _saleTime = 5, _tokenValue = 1,
+		_smallPurchase = 1, _medPurchase = 3, _bigPurchase = 5};
+	let launchNick, owner, buyerOne, buyerTwo, buyerThree, buyerFour;
 	// YOUR CODE HERE
 
 	/* Do something before every `describe` method */
 	beforeEach(async function() {
 		// YOUR CODE HERE
-	});
+		/* Launches a new ICO with the standard token value, sale time, and a limited supply of tokens. */ 
+		launchNick = await Crowdsale.new({value: _tokenValue, _saleTime, _totalSupply});
+		});
 
 	/* Group test cases together 
 	 * Make sure to provide descriptive strings for method arguements and
 	 * assert statements
 	 */
-	describe('Your string here', function() {
-		it("your string here", async function() {
+	describe('Crowdsale Works', function() {
+		it("Buyers can purchase a specified amount of tokens from the ICO owner", async function() {
 			// YOUR CODE HERE
+			let buyerOne = await Queue.enqueue(buyerOne);
+			let buyerTwo = await Queue.enqueue(buyerTwo);
+			let buyerThree = await Queue.enqueue(buyerThree);
+
+			let buyerOne = await Crowdsale.buy(_smallPurchase);
+			await Token.balanceOf(buyerOne);
+			let buyerTwo = await Crowdsale.buy(_smallPurchase);
+			await Token.balanceOf(buyerTwo);
+		});
+		it("Buyers can use tokens in transactions.", async function() {
+			// YOUR CODE HERE
+			let buyerOne = Token.transferTo(buyerTwo, 1);
+			assert.equal(Token.balanceOf(balanceTwo), 6);
+			let buyerTwo = Token.transferTo(buyerOne, 6);
+			assert.equal(Token.balanceOf(balanceOne), 6);
 		});
 		// YOUR CODE HERE
 	});
